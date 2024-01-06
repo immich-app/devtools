776133523c8bb32f01b013fe6d620d8ed725ae20
bo0tzz <git@bo0tzz.me>
2024-01-06 20:07:06 +0100

    Missing top level key

--- nixos/disk-config.nix
+++ nixos/disk-config.nix
@@ -1,75 +1,77 @@
 { lib, disks ? [ "/dev/disk/by-id/nvme-SAMSUNG_MZQLB1T9HAJR-00007_S439NC0R300444" "/dev/disk/by-id/nvme-SAMSUNG_MZQLB1T9HAJR-00007_S439NC0R303530" ], ... }: {
-  disk = lib.genAttrs disks (dev: {
-    device = dev;
-    type = "disk";
-    content = {
-      type = "table";
-      format = "gpt";
-      partitions = [
-        {
-          name = "boot";
-          start = "0";
-          end = "1MiB";
-          part-type = "primary";
-          flags = ["bios_grub"];
-        }
-        {
-          name = "ESP";
-          start = "1MiB";
-          end = "385MiB";
-          bootable = true;
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = if dev == "/dev/disk/by-id/nvme-SAMSUNG_MZQLB1T9HAJR-00007_S439NC0R300444" then "/boot" else null;
-          };
-        }
-        {
-          name = "zpool";
-          start = "385MiB";
-          end = "100%";
-          content = {
-            type = "zfs";
-            pool = "zpool";
-          };
-        }
-      ];
-    };
-  });
-  zpool = {
+  disko.devices = {
+    disk = lib.genAttrs disks (dev: {
+      device = dev;
+      type = "disk";
+      content = {
+        type = "table";
+        format = "gpt";
+        partitions = [
+          {
+            name = "boot";
+            start = "0";
+            end = "1MiB";
+            part-type = "primary";
+            flags = ["bios_grub"];
+          }
+          {
+            name = "ESP";
+            start = "1MiB";
+            end = "385MiB";
+            bootable = true;
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = if dev == "/dev/disk/by-id/nvme-SAMSUNG_MZQLB1T9HAJR-00007_S439NC0R300444" then "/boot" else null;
+            };
+          }
+          {
+            name = "zpool";
+            start = "385MiB";
+            end = "100%";
+            content = {
+              type = "zfs";
+              pool = "zpool";
+            };
+          }
+        ];
+      };
+    });
     zpool = {
-      type = "zpool";
-      mode = "mirror";
-      options = {
-        ashift = "12";
-        autotrim = "on";
-      };
-      rootFsOptions = {
-        mountpoint = "none";
-        acltype = "posixacl";
-        canmount = "off";
-        compression = "zstd";
-        dnodesize = "auto";
-        normalization = "formD";
-        relatime = "on";
-        xattr = "sa";
-      };
+      zpool = {
+        type = "zpool";
+        mode = "mirror";
+        options = {
+          ashift = "12";
+          autotrim = "on";
+        };
+        rootFsOptions = {
+          mountpoint = "none";
+          acltype = "posixacl";
+          canmount = "off";
+          compression = "zstd";
+          dnodesize = "auto";
+          normalization = "formD";
+          relatime = "on";
+          xattr = "sa";
+        };
 
-      datasets = {
-        root = {
-          type = "zfs_fs";
-          options.mountpoint = "legacy";
-          mountpoint = "/";
-        };
-        nix = {
-          type = "zfs_fs";
-          options.mountpoint = "legacy";
-          mountpoint = "/nix";
-        };
-        home = {
-          type = "zfs_fs";
-          options.mountpoint = "legacy";
-          mountpoint = "/home";
+        datasets = {
+          root = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            mountpoint = "/";
+          };
+          nix = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            mountpoint = "/nix";
+          };
+          home = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            mountpoint = "/home";
+          };
         };
       };
     };
