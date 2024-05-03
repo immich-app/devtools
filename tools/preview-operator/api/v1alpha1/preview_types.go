f2b38f73ec166620745a8063cb2a5b8acc06dc2c
bo0tzz <git@bo0tzz.me>
2024-05-03 13:54:13 +0000

    feat(operator): Initial PreviewSpec fields

--- tools/preview-operator/api/v1alpha1/preview_types.go
+++ tools/preview-operator/api/v1alpha1/preview_types.go
@@ -4,22 +4,36 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+type ImmichConfiguration struct {
+	Tag string `json:"tag"`
+}
+
+type DatabaseInitType string
+
+const (
+	DatabaseInitTypeEmpty DatabaseInitType = "empty"
+)
+
+type DatabaseConfiguration struct {
+	// +kubebuilder:default:=empty
+	// +kubebuilder:validation:Enum:=empty
+	// +optional
+	InitType DatabaseInitType `json:"initType,omitempty"`
+}
 
 // PreviewSpec defines the desired state of Preview
 type PreviewSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Preview. Edit preview_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Immich *ImmichConfiguration `json:"immich"`
+
+	Database *DatabaseConfiguration `json:"database,omitempty"`
 }
 
 // PreviewStatus defines the observed state of Preview
 type PreviewStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
 }
 
 //+kubebuilder:object:root=true
