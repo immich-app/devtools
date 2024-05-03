113d165bd1c652dec31935d8b7f55119624bfcd0
bo0tzz <git@bo0tzz.me>
2024-05-03 18:16:16 +0000

    feat(operator): Abstract component reconciliation

--- tools/preview-operator/internal/controller/preview_controller.go
+++ tools/preview-operator/internal/controller/preview_controller.go
@@ -2,17 +2,10 @@ package controller
 
 import (
 	"context"
-	"fmt"
-	"strings"
-	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/api/meta"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -61,126 +54,26 @@ func (r *PreviewReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		return ctrl.Result{}, err
 	}
 
-	//Reconcile immich-server deployment
-	server := &appsv1.Deployment{}
-	err = r.Get(ctx, types.NamespacedName{Name: preview.Name, Namespace: preview.Namespace}, server)
-	if err != nil && apierrors.IsNotFound(err) {
-		// Define a new deployment
-		dep, err := r.deploymentForServer(preview)
-		if err != nil {
-			log.Error(err, "Failed to define new Deployment resource for immich-server")
-
-			// The following implementation will update the status
-			meta.SetStatusCondition(&preview.Status.Conditions, metav1.Condition{Type: typeAvailableDeployment,
-				Status: metav1.ConditionFalse, Reason: "Reconciling",
-				Message: fmt.Sprintf("Failed to create immich-server Deployment for the custom resource (%s): (%s)", preview.Name, err)})
-
-			if err := r.Status().Update(ctx, preview); err != nil {
-				log.Error(err, "Failed to update Preview status")
-				return ctrl.Result{}, err
-			}
-
-			return ctrl.Result{}, err
-		}
-
-		log.Info("Creating a new Deployment",
-			"Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
-		if err = r.Create(ctx, dep); err != nil {
-			log.Error(err, "Failed to create new Deployment",
-				"Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
-			return ctrl.Result{}, err
-		}
-
-		// Deployment created successfully
-		// We will requeue the reconciliation so that we can ensure the state
-		// and move forward for the next operations
-		return ctrl.Result{RequeueAfter: time.Minute}, nil
-	} else if err != nil {
-		log.Error(err, "Failed to get Deployment")
-		// Let's return the error for the reconciliation be re-trigged again
-		return ctrl.Result{}, err
-	}
-	meta.SetStatusCondition(&preview.Status.Conditions, metav1.Condition{Type: typeAvailableDeployment,
-		Status: metav1.ConditionTrue, Reason: "Reconciling",
-		Message: fmt.Sprintf("Deployment for custom resource (%s) created successfully", preview.Name)})
-
-	if err := r.Status().Update(ctx, preview); err != nil {
-		log.Error(err, "Failed to update Preview status")
-		return ctrl.Result{}, err
+	serverController := &ComponentController{
+		ComponentName: "immich-server",
+		Image:         "ghcr.io/immich-app/immich-server",
+		Port:          3001,
+		Args:          []string{"start.sh", "immich"},
 	}
 
-	return ctrl.Result{}, nil
+	res, err := serverController.Reconcile(ctx, r, preview)
+	if err != nil {
+		return res, err
+	}
+
+	//TODO: Figure out the details of what Result needs to be returned when
+	return res, err
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *PreviewReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&devtoolsv1alpha1.Preview{}).
+		Owns(&appsv1.Deployment{}).
 		Complete(r)
-}
-
-func nameForServer(preview *devtoolsv1alpha1.Preview) string {
-	return preview.Name + "-server"
-}
-
-func labelsForServer(preview *devtoolsv1alpha1.Preview) map[string]string {
-	var imageTag string
-	image := imageForServer(preview)
-	imageTag = strings.Split(image, ":")[1]
-	name := nameForServer(preview)
-
-	return map[string]string{"app.kubernetes.io/name": "immich-server",
-		"app.kubernetes.io/instance":   name,
-		"app.kubernetes.io/version":    imageTag,
-		"app.kubernetes.io/part-of":    "preview-operator",
-		"app.kubernetes.io/created-by": "controller-manager",
-	}
-}
-
-func imageForServer(preview *devtoolsv1alpha1.Preview) string {
-	tag := preview.Spec.Immich.Tag
-	return "ghcr.io/immich-app/immich-server:" + tag
-}
-
-func (r *PreviewReconciler) deploymentForServer(preview *devtoolsv1alpha1.Preview) (*appsv1.Deployment, error) {
-	ls := labelsForServer(preview)
-	image := imageForServer(preview)
-
-	replicas := preview.Spec.Immich.Server.Replicas
-
-	dep := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      preview.Name,
-			Namespace: preview.Namespace,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: &replicas,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: ls,
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: ls,
-				},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{{
-						Image:           image,
-						Name:            "immich-server",
-						ImagePullPolicy: corev1.PullAlways,
-						Ports: []corev1.ContainerPort{{
-							ContainerPort: 3001,
-							Name:          "http",
-						}},
-						Args: []string{"start.sh", "immich"},
-						//TODO: env, volume
-					}},
-				},
-			},
-		},
-	}
-
-	if err := ctrl.SetControllerReference(preview, dep, r.Scheme); err != nil {
-		return nil, err
-	}
-	return dep, nil
 }
