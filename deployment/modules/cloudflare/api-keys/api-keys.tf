c4fa4a9fada25515b44bbca54412ccb955208bf5
Zack Pollard <zackpollard@ymail.com>
2024-06-05 11:57:38 +0100

    feat: add r2 bucket and mich r2 cloudflare token

--- deployment/modules/cloudflare/api-keys/api-keys.tf
+++ deployment/modules/cloudflare/api-keys/api-keys.tf
@@ -9,6 +9,7 @@ resource "cloudflare_api_token" "terraform_cloudflare_account" {
       data.cloudflare_api_token_permission_groups.all.zone["Zone Write"],
       data.cloudflare_api_token_permission_groups.all.zone["Zone Settings Write"],
       data.cloudflare_api_token_permission_groups.all.zone["Dynamic URL Redirects Write"],
+      data.cloudflare_api_token_permission_groups.all.account["Workers R2 Storage Write"]
     ]
     resources = {
       "com.cloudflare.api.account.*" = "*"
@@ -53,5 +54,32 @@ resource "cloudflare_api_token" "terraform_cloudflare_pages_upload" {
 
 output "terraform_key_cloudflare_pages_upload" {
   value     = cloudflare_api_token.terraform_cloudflare_account.value
+  sensitive = true
+}
+
+resource "cloudflare_api_token" "mich_cloudflare_r2_token" {
+  name = "mich_r2_token"
+  policy {
+    permission_groups = [
+      data.cloudflare_api_token_permission_groups.all.r2["Workers R2 Storage Bucket Item Write"]
+    ]
+    resources = {
+      "com.cloudflare.edge.r2.bucket.*" = "*"
+    }
+  }
+  condition {
+    request_ip {
+      in = local.mich_cidrs
+    }
+  }
+}
+
+output "mich_cloudflare_r2_token_id" {
+  value     = cloudflare_api_token.mich_cloudflare_r2_token.id
+  sensitive = true
+}
+
+output "mich_cloudflare_r2_token_value" {
+  value     = cloudflare_api_token.mich_cloudflare_r2_token.value
   sensitive = true
 }
