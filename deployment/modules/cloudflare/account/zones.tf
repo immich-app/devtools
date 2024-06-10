cbf9886edbfdabbbb840eaa896c86f145aa63c6d
Zack Pollard <zackpollard@ymail.com>
2024-06-10 10:12:43 +0100

    feat: create cloudflare zone for *.immich.futo.org

--- deployment/modules/cloudflare/account/zones.tf
+++ deployment/modules/cloudflare/account/zones.tf
@@ -37,3 +37,23 @@ resource "cloudflare_zone_settings_override" "immich_cloud" {
 output "immich_cloud_zone_id" {
   value = cloudflare_zone.immich_cloud.id
 }
+
+resource "cloudflare_zone" "immich_futo_org" {
+  account_id = var.cloudflare_account_id
+  zone       = "immich.futo.org"
+}
+
+resource "cloudflare_zone_settings_override" "immich_futo_org" {
+  zone_id = cloudflare_zone.immich_futo_org.id
+
+  settings {
+    http3            = "on"
+    zero_rtt         = "on"
+    tls_1_3          = "zrt"
+    always_use_https = "on"
+  }
+}
+
+output "immich_futo_org_zone_id" {
+  value = cloudflare_zone.immich_futo_org.id
+}
