3537fe61f54350cee8941f9deb4c2fadf1b4625a
Zack Pollard <zackpollard@ymail.com>
2024-07-02 16:36:12 +0100

    chore: remove immich-app cloudflare pages project (#65)

--- deployment/modules/cloudflare/account/pages-project.tf
+++ deployment/modules/cloudflare/account/pages-project.tf
@@ -1,28 +1,15 @@
-resource "cloudflare_pages_project" "immich_app" {
-  account_id        = var.cloudflare_account_id
-  name              = "immich-app"
-  production_branch = "we-will-never-use-this"
-
-  lifecycle {
-    ignore_changes = [
-      build_config,
-    ]
-  }
-}
-
-output "immich_app_pages_project_name" {
-  value = cloudflare_pages_project.immich_app.name
-}
-
-output "immich_app_pages_project_subdomain" {
-  value = cloudflare_pages_project.immich_app.subdomain
-}
-
-
 resource "cloudflare_pages_project" "immich_app_archive" {
   account_id        = var.cloudflare_account_id
   name              = "immich-app-archive"
   production_branch = "we-will-never-use-this"
+
+  lifecycle {
+    ignore_changes = [
+      build_config["build_caching"],
+      build_config["web_analytics_tag"],
+      build_config["web_analytics_token"],
+    ]
+  }
 }
 
 output "immich_app_archive_pages_project_name" {
@@ -37,6 +24,14 @@ resource "cloudflare_pages_project" "immich_app_preview" {
   account_id        = var.cloudflare_account_id
   name              = "immich-app-preview"
   production_branch = "we-will-never-use-this"
+
+  lifecycle {
+    ignore_changes = [
+      build_config["build_caching"],
+      build_config["web_analytics_tag"],
+      build_config["web_analytics_token"],
+    ]
+  }
 }
 
 output "immich_app_preview_pages_project_name" {
@@ -51,6 +46,14 @@ resource "cloudflare_pages_project" "my_immich_app" {
   account_id        = var.cloudflare_account_id
   name              = "my-immich-app"
   production_branch = "main"
+
+  lifecycle {
+    ignore_changes = [
+      build_config["build_caching"],
+      build_config["web_analytics_tag"],
+      build_config["web_analytics_token"],
+    ]
+  }
 }
 
 output "my_immich_app_pages_project_name" {
