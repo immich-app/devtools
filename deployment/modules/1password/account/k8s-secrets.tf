9ba4e3a3f841870d4418a1da0d8ae9fcb6a863d9
bo0tzz <git@bo0tzz.me>
2024-07-25 17:23:28 +0200

    chore: clean up containerssh (#122)

--- deployment/modules/1password/account/k8s-secrets.tf
+++ deployment/modules/1password/account/k8s-secrets.tf
@@ -6,28 +6,6 @@ data "onepassword_vault" "kubernetes" {
   name = "Kubernetes"
 }
 
-resource "random_password" "containerssh_oauth_secret" {
-  length           = 20
-  special          = true
-  override_special = "!@#$%^&*()_+"
-}
-
-resource "onepassword_item" "containerssh_oauth" {
-  vault    = data.onepassword_vault.kubernetes.uuid
-  title    = "containerssh-oauth"
-  category = "secure_note"
-
-  section {
-    label = "oauth secret"
-
-    field {
-      label = "CONTAINERSSH_OAUTH_SECRET"
-      type  = "CONCEALED"
-      value = random_password.containerssh_oauth_secret.result
-    }
-  }
-}
-
 resource "tls_private_key" "containerssh_host_key" {
   algorithm = "ED25519"
 }
