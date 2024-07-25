f03154c39fbfea2fbbc74ea7003c25bfec9d0e95
bo0tzz <git@bo0tzz.me>
2024-07-25 15:37:34 +0200

    feat: deploy containerssh (#115)

--- deployment/modules/1password/account/k8s-secrets.tf
+++ deployment/modules/1password/account/k8s-secrets.tf
@@ -6,36 +6,44 @@ data "onepassword_vault" "kubernetes" {
   name = "Kubernetes"
 }
 
-resource "random_password" "example_k8s_password_gen_string_2" {
+resource "random_password" "containerssh_oauth_secret" {
   length           = 20
   special          = true
   override_special = "!@#$%^&*()_+"
 }
 
-resource "onepassword_item" "example_k8s_password_gen" {
+resource "onepassword_item" "containerssh_oauth" {
   vault    = data.onepassword_vault.kubernetes.uuid
-  title    = "example-terraform-k8s-password-gen"
+  title    = "containerssh-oauth"
   category = "password"
-  password_recipe {
-    length  = 40
-    symbols = true
-    letters = true
-    digits  = true
-  }
 
   section {
-    label = "Example custom section for terraform k8s vault item"
+    label = ""
 
     field {
-      label = "string-field"
-      type  = "STRING"
-      value = "example"
+      label = "CONTAINERSSH_OAUTH_SECRET"
+      type  = "CONCEALED"
+      value = random_password.containerssh_oauth_secret.result
     }
+  }
+}
+
+resource "tls_private_key" "containerssh_host_key" {
+  algorithm = "ED25519"
+}
+
+resource "onepassword_item" "containerssh_host_key" {
+  vault    = data.onepassword_vault.kubernetes.uuid
+  title    = "containerssh-host-key"
+  category = "password"
+
+  section {
+    label = ""
 
     field {
-      label = "string-field-generated"
-      type  = "STRING"
-      value = random_password.example_k8s_password_gen_string_2.result
+      label = "host.key"
+      type  = "CONCEALED"
+      value = tls_private_key.containerssh_host_key.private_key_openssh
     }
   }
 }
