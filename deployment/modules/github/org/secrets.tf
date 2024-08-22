140c20c5399c1676ab693fd727c584526a0376b3
Zack Pollard <zackpollard@ymail.com>
2024-08-22 13:45:01 +0100

    feat: kv r2 tile server token

--- deployment/modules/github/org/secrets.tf
+++ deployment/modules/github/org/secrets.tf
@@ -3,3 +3,22 @@ resource "github_actions_organization_secret" "cloudflare_api_token_pages_upload
   plaintext_value = data.terraform_remote_state.api_keys_state.outputs.terraform_key_cloudflare_pages_upload
   visibility      = "all"
 }
+
+resource "github_actions_organization_secret" "tiles_r2_kv_token_id" {
+  secret_name     = "CLOUDFLARE_TILES_R2_KV_TOKEN_ID"
+  plaintext_value = data.terraform_remote_state.api_keys_state.outputs.tiles_r2_kv_token_id
+  visibility      = "all"
+}
+
+resource "github_actions_organization_secret" "tiles_r2_kv_token_value" {
+  secret_name     = "CLOUDFLARE_TILES_R2_KV_TOKEN_VALUE"
+  plaintext_value = data.terraform_remote_state.api_keys_state.outputs.tiles_r2_kv_token_value
+  visibility      = "all"
+}
+
+resource "github_actions_organization_secret" "tiles_r2_kv_token_hashed_value" {
+  secret_name     = "CLOUDFLARE_TILES_R2_KV_TOKEN_HASHED_VALUE"
+  plaintext_value = sha256(data.terraform_remote_state.api_keys_state.outputs.tiles_r2_kv_token_value)
+  visibility      = "all"
+}
+
