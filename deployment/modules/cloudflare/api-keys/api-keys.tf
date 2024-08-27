a25a489813690136d159350d2481deb52aad8ee8
Zack Pollard <zackpollard@ymail.com>
2024-08-27 16:22:52 +0100

    feat: kv r2 tile server token (#172)
    * feat: kv r2 tile server token
    
    * feat: push-o-matic

--- deployment/modules/cloudflare/api-keys/api-keys.tf
+++ deployment/modules/cloudflare/api-keys/api-keys.tf
@@ -99,3 +99,37 @@ output "mich_cloudflare_r2_token_value" {
   value     = cloudflare_api_token.mich_cloudflare_r2_token.value
   sensitive = true
 }
+
+resource "cloudflare_api_token" "tiles_r2_kv_token" {
+  name = "tiles_r2_kv_token"
+  policy {
+    permission_groups = [
+      data.cloudflare_api_token_permission_groups.all.account["Workers R2 Storage Read"],
+      data.cloudflare_api_token_permission_groups.all.account["Workers R2 Storage Write"],
+      data.cloudflare_api_token_permission_groups.all.account["Workers KV Storage Read"],
+      data.cloudflare_api_token_permission_groups.all.account["Workers KV Storage Write"]
+    ]
+    resources = {
+      "com.cloudflare.api.account.*" = "*"
+    }
+  }
+  policy {
+    permission_groups = [
+      data.cloudflare_api_token_permission_groups.all.r2["Workers R2 Storage Bucket Item Read"],
+      data.cloudflare_api_token_permission_groups.all.r2["Workers R2 Storage Bucket Item Write"]
+    ]
+    resources = {
+      "com.cloudflare.edge.r2.bucket.*" = "*"
+    }
+  }
+}
+
+output "tiles_r2_kv_token_id" {
+  value     = cloudflare_api_token.tiles_r2_kv_token.id
+  sensitive = true
+}
+
+output "tiles_r2_kv_token_value" {
+  value     = cloudflare_api_token.tiles_r2_kv_token.value
+  sensitive = true
+}
--- deployment/modules/github/org/secrets.tf
+++ deployment/modules/github/org/secrets.tf
@@ -1,5 +1,57 @@
+data "onepassword_vault" "github" {
+  name = "Github"
+}
+
 resource "github_actions_organization_secret" "cloudflare_api_token_pages_upload" {
   secret_name     = "CLOUDFLARE_API_TOKEN_PAGES_UPLOAD"
   plaintext_value = data.terraform_remote_state.api_keys_state.outputs.terraform_key_cloudflare_pages_upload
+  visibility      = "all"
+}
+
+resource "github_actions_organization_secret" "tiles_r2_kv_token_id" {
+  secret_name     = "CLOUDFLARE_TILES_R2_KV_TOKEN_ID"
+  plaintext_value = data.terraform_remote_state.api_keys_state.outputs.tiles_r2_kv_token_id
+  visibility      = "all"
+}
+
+resource "github_actions_organization_secret" "tiles_r2_kv_token_value" {
+  secret_name     = "CLOUDFLARE_TILES_R2_KV_TOKEN_VALUE"
+  plaintext_value = data.terraform_remote_state.api_keys_state.outputs.tiles_r2_kv_token_value
+  visibility      = "all"
+}
+
+resource "github_actions_organization_secret" "tiles_r2_kv_token_hashed_value" {
+  secret_name     = "CLOUDFLARE_TILES_R2_KV_TOKEN_HASHED_VALUE"
+  plaintext_value = sha256(data.terraform_remote_state.api_keys_state.outputs.tiles_r2_kv_token_value)
+  visibility      = "all"
+}
+
+data "onepassword_item" "push_o_matic_app" {
+  title = "push-o-matic-app"
+  vault = data.onepassword_vault.github.name
+}
+
+locals {
+  push_o_matic_fields = {
+    app_id          = [for field in data.onepassword_item.push_o_matic_app.section[0].field : field.value if field.label == "app_id"][0]
+    installation_id = [for field in data.onepassword_item.push_o_matic_app.section[0].field : field.value if field.label == "installation_id"][0]
+  }
+}
+
+resource "github_actions_organization_secret" "push_o_matic_app_id" {
+  secret_name     = "PUSH_O_MATIC_APP_ID"
+  plaintext_value = local.push_o_matic_fields.app_id
+  visibility      = "all"
+}
+
+resource "github_actions_organization_secret" "push_o_matic_app_installation_id" {
+  secret_name     = "PUSH_O_MATIC_APP_INSTALLATION_ID"
+  plaintext_value = local.push_o_matic_fields.installation_id
+  visibility      = "all"
+}
+
+resource "github_actions_organization_secret" "push_o_matic_app_key" {
+  secret_name     = "PUSH_O_MATIC_APP_KEY"
+  plaintext_value = data.onepassword_item.push_o_matic_app.private_key
   visibility      = "all"
 }
