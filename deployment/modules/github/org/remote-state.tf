e88ca38a538d8cbf550b94b8c4962ad085464c52
Daniel Dietzler <mail@ddietzler.dev>
2024-09-19 20:04:46 +0200

    feat: bot webhooks

--- deployment/modules/github/org/remote-state.tf
+++ deployment/modules/github/org/remote-state.tf
@@ -6,3 +6,15 @@ data "terraform_remote_state" "api_keys_state" {
     schema_name = "prod_cloudflare_api_keys"
   }
 }
+
+data "onepassword_vault" "opentofu_vault" {
+  name = "OpenTofu"
+}
+
+data "onepassword_vault" "kubernetes" {
+  name = "Kubernetes"
+}
+
+data "onepassword_vault" "github" {
+  name = "Github"
+}
--- deployment/modules/github/org/secrets.tf
+++ deployment/modules/github/org/secrets.tf
@@ -1,7 +1,3 @@
-data "onepassword_vault" "github" {
-  name = "Github"
-}
-
 resource "github_actions_organization_secret" "cloudflare_api_token_pages_upload" {
   secret_name     = "CLOUDFLARE_API_TOKEN_PAGES_UPLOAD"
   plaintext_value = data.terraform_remote_state.api_keys_state.outputs.terraform_key_cloudflare_pages_upload
