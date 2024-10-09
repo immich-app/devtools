9a4c0312480a2555f71dbe52a0b393ce42897cf4
bo0tzz <git@bo0tzz.me>
2024-10-09 12:57:14 +0200

    feat: oauth login for grafana (#219)

--- deployment/modules/1password/account/k8s-secrets.tf
+++ deployment/modules/1password/account/k8s-secrets.tf
@@ -116,3 +116,24 @@ resource "onepassword_item" "hedgedoc_oauth_secret" {
     }
   }
 }
+
+resource "random_password" "grafana_oauth_client_secret" {
+  length  = 40
+  special = false
+}
+
+resource "onepassword_item" "grafana_oauth_client_secret" {
+  vault    = data.onepassword_vault.kubernetes.uuid
+  title    = "grafana-oauth-client-secret"
+  category = "secure_note"
+
+  section {
+    label = "OAuth secret for grafana"
+
+    field {
+      label = "GRAFANA_OAUTH_CLIENT_SECRET"
+      type  = "CONCEALED"
+      value = random_password.grafana_oauth_client_secret.result
+    }
+  }
+}
