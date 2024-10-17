9c34eb1c2e402b631db5761883861fd6223072d7
Zack Pollard <zack@futo.org>
2024-10-17 20:52:40 +0200

    feat: docker hub org terraform

---
+++ deployment/modules/docker/org/config.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "pg" {
+    schema_name = "prod_docker_org"
+  }
+  required_version = "~> 1.7"
+
+  required_providers {
+    docker = {
+      source  = "registry.terraform.io/docker/docker"
+      version = "0.2.1"
+    }
+    onepassword = {
+      source  = "1Password/onepassword"
+      version = "~> 2.1"
+    }
+  }
+}
