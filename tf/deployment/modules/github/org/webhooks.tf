7cd141323306d083aced36c300374309e5f3a504
bo0tzz <git@bo0tzz.me>
2025-03-05 13:56:47 +0100

    feat: configure github webhook for previews

--- tf/deployment/modules/github/org/webhooks.tf
+++ tf/deployment/modules/github/org/webhooks.tf
@@ -15,3 +15,25 @@ resource "github_organization_webhook" "bot" {
     content_type = "json"
   }
 }
+
+data "onepassword_item" "previews_webhook_secret" {
+  title = "previews-webhook-secret"
+  vault = data.onepassword_vault.kubernetes.name
+}
+
+locals {
+  previews_webhook_token = [for field in data.onepassword_item.previews_webhook_secret.section[0].field : field.value if field.label == "token"][0]
+}
+
+resource "github_repository_webhook" "previews" {
+  events = [
+    "push",
+    "pull_request"
+  ]
+  repository = "immich"
+  configuration {
+    url          = data.onepassword_item.previews_webhook_secret.url
+    secret       = local.previews_webhook_token
+    content_type = "form"
+  }
+}
