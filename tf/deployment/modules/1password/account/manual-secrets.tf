d8bf5f5a13292c46e2f8df3b1e40f72738dc7204
Zack Pollard <zackpollard@ymail.com>
2025-04-04 15:45:21 +0100

    feat: allow generated secrets to be specified and create discord bot secrets

--- tf/deployment/modules/1password/account/manual-secrets.tf
+++ tf/deployment/modules/1password/account/manual-secrets.tf
@@ -1,60 +1,76 @@
-data "onepassword_vault" "manual" {
-  name = "tf_manual"
-}
-
-data "onepassword_vault" "manual_dev" {
-  name = "tf_dev_manual"
-}
-
-data "onepassword_vault" "manual_prod" {
-  name = "tf_prod_manual"
-}
-
-variable "repositories" {
+variable "manual_secrets" {
   type = object({
     secrets      = list(string)
     dev_secrets  = list(string)
     prod_secrets = list(string)
   })
   default = {
-    secrets      = ["example"]
+    secrets = [
+      "IMMICH_DISCORD_BOT_TOKEN",
+      "IMMICH_ZULIP_BOT_USERNAME",
+      "IMMICH_ZULIP_BOT_API_KEY",
+      "IMMICH_GITHUB_CLIENT_ID",
+      "IMMICH_GITHUB_CLIENT_SECRET",
+      "FUTO_ZULIP_DOMAIN",
+      "FOURTHWALL_USER",
+      "FOURTHWALL_PASSWORD"
+    ]
     dev_secrets  = ["example"]
     prod_secrets = ["example"]
   }
 }
 
 locals {
-  all_secrets = concat(
+  manual_secrets = concat(
     [
-      for name in var.repositories.secrets : {
-        vault = data.onepassword_vault.manual
-        name  = name
+      for name in var.manual_secrets.secrets : {
+        manual_vault = data.onepassword_vault.manual
+        vault        = data.onepassword_vault.tf
+        name         = name
       }
     ],
     [
-      for name in var.repositories.dev_secrets : {
-        vault = data.onepassword_vault.manual_dev
-        name  = name
+      for name in var.manual_secrets.dev_secrets : {
+        manual_vault = data.onepassword_vault.manual_dev
+        vault        = data.onepassword_vault.tf_dev
+        name         = name
       }
     ],
     [
-      for name in var.repositories.prod_secrets : {
-        vault = data.onepassword_vault.manual_prod
-        name  = name
+      for name in var.manual_secrets.prod_secrets : {
+        manual_vault = data.onepassword_vault.manual_prod
+        vault        = data.onepassword_vault.tf_prod
+        name         = name
       }
     ]
   )
 }
 
 resource "onepassword_item" "manual" {
-  for_each = { for idx, secret in local.all_secrets : "${secret.vault.name}_${secret.name}" => secret }
+  for_each = { for idx, secret in local.manual_secrets : "${secret.vault.name}_${secret.name}" => secret }
 
-  vault    = each.value.vault.uuid
+  vault    = each.value.manual_vault.uuid
   title    = each.value.name
   category = "password"
   password = "REPLACE_ME"
 
   lifecycle {
     ignore_changes = [password]
+  }
+}
+
+resource "onepassword_item" "copy" {
+  for_each = { for idx, secret in local.manual_secrets : "${secret.vault.name}_${secret.name}" => secret }
+
+  vault    = each.value.vault.uuid
+  title    = each.value.name
+  category = "password"
+  password = onepassword_item.manual[each.key].password
+}
+
+output "manual_secret_values" {
+  value = {
+    for secret in local.manual_secrets :
+    secret.name => onepassword_item.copy[format("${secret.vault.name}_${secret.name}")].password
   }
 }
