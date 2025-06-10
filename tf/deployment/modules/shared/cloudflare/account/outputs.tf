2b0709cd70d7b6117ce9a7fad8d11d1f2f216390
bwees <brandonwees@gmail.com>
2025-06-10 10:44:09 -0500

    fix: formatting

--- tf/deployment/modules/shared/cloudflare/account/outputs.tf
+++ tf/deployment/modules/shared/cloudflare/account/outputs.tf
@@ -3,10 +3,10 @@ output "cloudflare_account_id" {
 }
 
 output "turnstile_default_invisible_secret" {
-  value = cloudflare_turnstile_widget.default_invisible.secret
+  value     = cloudflare_turnstile_widget.default_invisible.secret
   sensitive = true
 }
 
 output "turnstile_default_invisible_site_key" {
   value = cloudflare_turnstile_widget.default_invisible.id
-}
+} 
