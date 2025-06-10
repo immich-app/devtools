20e7448af98819ee017388aacc6abb640fc7171e
Zack Pollard <zackpollard@ymail.com>
2025-06-10 12:46:54 +0100

    feat: pages project production branch should match env type

--- tf/shared/modules/cloudflare-pages-project/project.tf
+++ tf/shared/modules/cloudflare-pages-project/project.tf
@@ -1,10 +1,10 @@
 resource "cloudflare_pages_project" "project" {
   account_id        = var.cloudflare_account_id
   name              = local.sanitised_project_name
-  production_branch = "prod"
+  production_branch = var.env
 
   build_config {
-    web_analytics_tag = cloudflare_web_analytics_site.analytics.site_tag
+    web_analytics_tag   = cloudflare_web_analytics_site.analytics.site_tag
     web_analytics_token = cloudflare_web_analytics_site.analytics.site_token
   }
 
@@ -23,11 +23,11 @@ module "domain" {
   source = "../domain"
 
   app_name = var.app_name
-  env = var.env
+  env      = var.env
 }
 
 resource "cloudflare_web_analytics_site" "analytics" {
-  account_id = var.cloudflare_account_id
-  host = module.domain.fqdn
+  account_id   = var.cloudflare_account_id
+  host         = module.domain.fqdn
   auto_install = false
 }
