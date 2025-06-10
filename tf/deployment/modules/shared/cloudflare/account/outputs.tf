9fe9a2fbf1b0939ee7a17acc22e709afa5638ace
Brandon Wees <brandonwees@gmail.com>
2025-06-10 10:46:14 -0500

    feat: cloudflare turnstile deployment (#726)

--- tf/deployment/modules/shared/cloudflare/account/outputs.tf
+++ tf/deployment/modules/shared/cloudflare/account/outputs.tf
@@ -1,3 +1,12 @@
 output "cloudflare_account_id" {
   value = var.cloudflare_account_id
 }
+
+output "turnstile_default_invisible_secret" {
+  value     = cloudflare_turnstile_widget.default_invisible.secret
+  sensitive = true
+}
+
+output "turnstile_default_invisible_site_key" {
+  value = cloudflare_turnstile_widget.default_invisible.id
+} 
