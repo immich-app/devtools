ef79b233a3ba660cdb9b2b9c1a98887b728f8956
Zack Pollard <zackpollard@ymail.com>
2025-07-08 18:00:08 +0100

    feat: org wide approval checks

--- tf/deployment/modules/shared/github/org/rulesets.tf
+++ tf/deployment/modules/shared/github/org/rulesets.tf
@@ -32,3 +32,40 @@ resource "github_organization_ruleset" "tag_restrictions" {
     update   = true
   }
 }
+
+resource "github_organization_ruleset" "org_required_checks" {
+  name        = "Org Required Checks"
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+    repository_name {
+      include = ["~ALL"]
+      exclude = []
+    }
+  }
+
+  bypass_actors {
+    actor_id    = 912027 # Immich Tofu Integration App
+    actor_type  = "Integration"
+    bypass_mode = "always"
+  }
+
+  bypass_actors {
+    actor_id    = 977022 # Immich Push-o-Matic Integration App
+    actor_type  = "Integration"
+    bypass_mode = "always"
+  }
+
+  rules {
+    required_status_checks {
+      required_check {
+        context = "Check for Team/Admin Review / check-approval"
+      }
+    }
+  }
+}
