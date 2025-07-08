6598c2c624e550f3d8f22cf38da7e765639dabbf
Zack Pollard <zackpollard@ymail.com>
2025-07-08 17:35:53 +0100

    feat: tags ruleset protection

---
+++ tf/deployment/modules/shared/github/org/rulesets.tf
@@ -0,0 +1,34 @@
+resource "github_organization_ruleset" "tag_restrictions" {
+  name        = "Pushing Tags Restrictions"
+  target      = "tag"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~ALL"]
+      exclude = []
+    }
+    repository_name {
+      include = ["~ALL"]
+      exclude = []
+    }
+  }
+
+  bypass_actors {
+    actor_id    = 912027 # Immich Tofu Integration App
+    actor_type  = "Integration"
+    bypass_mode = "always"
+  }
+
+  bypass_actors {
+    actor_id    = 977022 # Immich Push-o-Matic Integration App
+    actor_type  = "Integration"
+    bypass_mode = "always"
+  }
+
+  rules {
+    creation = true
+    deletion = true
+    update   = true
+  }
+}
