78c35b81bd2676c14272a8b042c9561a9474d3de
bo0tzz <git@bo0tzz.me>
2025-07-09 17:24:51 +0200

    fix: use privileged cf api token to create bucket tokens (#841)

--- tf/deployment/modules/shared/cloudflare/account/providers.tf
+++ tf/deployment/modules/shared/cloudflare/account/providers.tf
@@ -2,6 +2,11 @@ provider "cloudflare" {
   api_token = data.terraform_remote_state.api_keys_state.outputs.terraform_key_cloudflare_account
 }
 
+provider "cloudflare" {
+  alias     = "api_keys"
+  api_token = var.cloudflare_api_token
+}
+
 provider "onepassword" {
   service_account_token = var.op_service_account_token
 }
--- tf/deployment/modules/shared/cloudflare/account/variables.tf
+++ tf/deployment/modules/shared/cloudflare/account/variables.tf
@@ -1,3 +1,4 @@
 variable "cloudflare_account_id" {}
+variable "cloudflare_api_token" {}
 variable "tf_state_postgres_conn_str" {}
 variable "op_service_account_token" {}
