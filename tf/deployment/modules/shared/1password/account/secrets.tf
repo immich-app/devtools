f31f7f981136f1727268c57119cad8a45aec989f
bo0tzz <git@bo0tzz.me>
2025-09-01 17:14:21 +0200

    feat: deploy oauth2-proxy

--- tf/deployment/modules/shared/1password/account/secrets.tf
+++ tf/deployment/modules/shared/1password/account/secrets.tf
@@ -46,7 +46,8 @@ module "generated-secrets" {
       { name = "OUTLINE_SECRET_KEY", length = 64, type = "numeric" },
       { name = "OUTLINE_UTILS_SECRET" },
       { name = "OUTLINE_VOLSYNC_BACKUPS_RESTIC_SECRET" },
-      { name = "VICTORIALOGS_VOLSYNC_BACKUPS_RESTIC_SECRET" }
+      { name = "VICTORIALOGS_VOLSYNC_BACKUPS_RESTIC_SECRET" },
+      { name = "OAUTH2_PROXY_COOKIE_SECRET" }
     ]
     dev = [
       { name = "METRICS_READ_TOKEN" },
