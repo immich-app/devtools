8909941e41fb9ce28eba0e00ec8cb07f7c0d7d15
Zack Pollard <zackpollard@ymail.com>
2025-09-11 16:44:29 +0100

    feat: update domain and pages-project module to handle root app_name

--- tf/shared/modules/cloudflare-pages-project/locals.tf
+++ tf/shared/modules/cloudflare-pages-project/locals.tf
@@ -1,5 +1,6 @@
 locals {
-  app_name               = replace(var.app_name, "/[^a-zA-Z\\d]/", "-")
+  dashed_app_name        = replace(var.app_name, "/[^a-zA-Z\\d]/", "-")
   dashed_domain          = replace(var.domain, "/[^a-zA-Z\\d]/", "-")
-  sanitised_project_name = "${local.app_name}-${local.dashed_domain}-${var.env}"
+  app_name_prefix        = local.dashed_app_name == "root" ? "" : "${local.dashed_app_name}-"
+  sanitised_project_name = "${local.app_name_prefix}${local.dashed_domain}-${var.env}"
 }
--- tf/shared/modules/domain/outputs.tf
+++ tf/shared/modules/domain/outputs.tf
@@ -4,9 +4,11 @@ locals {
   // We don't include the environment name in the URL for prod
   domain_env = var.env == "prod" ? "" : "${var.env}."
   // Combine domain stage and environment, if stage is blank and env is prod, this will be an empty string
-  domain_prefix = "${local.domain_stage}${local.domain_env}"
+  domain_prefix   = "${local.domain_stage}${local.domain_env}"
+  dashed_app_name = replace(var.app_name, "/[^a-zA-Z\\d]/", "-")
+  domain_app_name = local.dashed_app_name == "root" ? "" : "${local.dashed_app_name}."
   // Example: buy.immich.app or buy.dev.immich.app or buy.pr-55.dev.immich.app
-  fqdn = "${var.app_name}.${local.domain_prefix}${var.domain}"
+  fqdn = "${local.domain_app_name}${local.domain_prefix}${var.domain}"
 }
 
 output "fqdn" {
