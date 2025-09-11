70d9991a0855bd5b36be9c130d0cba8a2bab329e
Zack Pollard <zackpollard@ymail.com>
2025-09-11 16:26:04 +0100

    feat: new redirects

--- tf/deployment/modules/shared/cloudflare/account/redirects.tf
+++ tf/deployment/modules/shared/cloudflare/account/redirects.tf
@@ -16,8 +16,24 @@ resource "cloudflare_ruleset" "immich_app_redirects" {
         preserve_query_string = true
       }
     }
-    expression  = "(http.host eq \"docs.immich.app\") or (http.host eq \"documentation.immich.app\") or (http.host eq \"www.immich.app\")"
-    description = "Redirect visitors going to docs or www"
+    expression  = "http.host eq \"www.immich.app\""
+    description = "Redirect visitors going to www.immich.app"
+    enabled     = true
+  }
+
+  rules {
+    action = "redirect"
+    action_parameters {
+      from_value {
+        status_code = 307
+        target_url {
+          value = "https://docs.immich.app"
+        }
+        preserve_query_string = true
+      }
+    }
+    expression  = "http.host eq \"documentation.immich.app\""
+    description = "Redirect visitors going to documentation.immich.app"
     enabled     = true
   }
 
